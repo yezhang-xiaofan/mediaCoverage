fcf6ede0d6ed9bf4bb567bcab8bfd896d46bd466
yezhang <yezhang@andrew.cmu.edu>
2015-04-12 15:51:35 -0500

    use word2Vec model to label Oxford press release

--- preprocessing_Oxford.py
+++ preprocessing_Oxford.py
@@ -4,33 +4,79 @@ import nltk
 import pickle
 import nltk
 import re
+from gensim.models.word2vec import Word2Vec
+import numpy as np
+from sets import Set
+from nltk.tokenize import word_tokenize
 #This file preprocesses Oxford health research related press release into sentences
 #And it label sentences as positive if it contains terms in the dictionary
 root = "PressRelease_Oxford/"
+model = Word2Vec.load('/scratch/cluster/yezhang/bio_word',mmap='r')
 dict = pickle.load(open("relation_terms","rb"))
 stopwords = nltk.corpus.stopwords.words('english')
-more_stopwords = ["have","more"]
-stopwords += more_stopwords
+#more_stopwords = ["have","more"]
+#stopwords += more_stopwords
+threshold = 0.25     #if the distance between any of the token in the sentence and any of the
+#IV/DV is less than threshold, then label as 1
+#remove stopwords
 for s in stopwords:
     if s in dict:
         del dict[s]
+
+#load IV/DV terms
+file = open("IV_DV.txt",'rb')
+IV_DV = {}
+def average_vector(term):
+    sum = np.zeros(200)
+    length = 0.0
+    word_list = word_tokenize(term)
+    for w in word_list:
+	   if(w.lower() in model):
+	       sum += model[w.lower()]
+           length += 1
+    if length==0.0:
+       print term
+       return None
+    return sum/length
+
+for line in file:
+    line = line.strip()
+    IV_DV[line] = average_vector(line)
+
+def check_term(sentence):
+    tokens = word_tokenize(sentence)
+    for IVDV in IV_DV.keys():
+        for t in tokens:
+            if t in stopwords:
+                continue
+            if(IV_DV[IVDV] is not None and t in model):
+                if(model.similarity(IV_DV[IVDV],t)<=threshold):
+                    return True
+    return False
+
 def process(file):
     write_file = open("PR_Oxford_Sentence/"+file,"w")
     file = open(root+file,'r')
     first_line = file.readline()
+    #check title
     title = first_line.split('|')[0]
     label = 0
     for term in dict:
         pattern = r'\b'+term+r'\b'
         if(re.search(pattern,title)):
             #print term
-            #print title
+                #print title
             label = 1
             break
+    if(label==1):
+        if(check_term(title)==False):
+            label = 0
     write_file.write(title+" "+str(label)+"\n")
     date = file.readline()
     #write_file.write(title.strip()+"\n")
     sentences = []
+
+    #check main body
     line = file.readline()
     while (line):
         cur_sen = nltk.sent_tokenize(line)
@@ -41,11 +87,12 @@ def process(file):
         for term in dict:
             pattern = r'\b'+term+r'\b'
             if(re.search(pattern,s)):
-                #print term
-                #print s
                 label = 1
                 break
-        write_file.write(s.strip()+" "+str(label)+"\n")
+        if(label==1):
+            if(check_term(s)==False):
+                label = 0
+        write_file.write(s.strip()+"\t"+str(label)+"\n")
     file.close()
     write_file.close()
 for file in os.listdir(root):
